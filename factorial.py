5e20c12ab60dbf5a7d050cbb12b3edefa694d3a9
VaNiesha Honani <vhonani77@gmail.com>
2025-11-12 18:42:55 -0800

    algo-factorial-py assignment - recursive

--- factorial.py
+++ factorial.py
@@ -1,3 +1,7 @@
 def factorial(num):
-	# your code here
-	pass
+	if num < 0:
+		return "This function only accepts non-negative numbers"
+	elif num == 0 or num == 1:
+		return 1
+	
+	return num * factorial(num-1)
